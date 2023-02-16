e6d648d66b1236340395fba6544a3a12b8ee1f2e
kwesisam <kwesisam270@gmail.com>
2023-02-16 10:15:19 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include <std.h>
+#include <stdio.h>
 /**
  * main - main block
  * return : 0
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * return : 0
+ */
+int main (void)
+{
+retunr (0);
+}
