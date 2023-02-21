d504955beba488162c57346ca1f039f09c8d0f0b
kwesisam <kwesisam270@gmail.com>
2023-02-20 23:02:12 -0800

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+ines (10 sloc)  149 Bytes
+
+#include "main.h"
+
+/**
+ * _abs - return abs value
+ * @n : number to check
+ * Return:0 or 1
+ */
+
+int _abs(int n)
+{
+
+return (n >= 0 ? n : n * -1);
+
+}
